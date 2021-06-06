0689ef49467ac7f08c0ed5bb8fe8cf07f046d8d7
Michael Stivens Pardo Burbano <programador2@sypsolutions.com>
2021-06-05 19:52:21 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
