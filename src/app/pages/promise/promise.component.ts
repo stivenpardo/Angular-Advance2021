76f987dc253bffcc4e6e0a4deb2ba54f50d09f24
Michael Stivens Pardo Burbano <programador2@sypsolutions.com>
2021-06-07 11:33:27 -0500

    example promise basic

---
+++ src/app/pages/promise/promise.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promise',
+  templateUrl: './promise.component.html',
+  styles: [
+  ]
+})
+export class PromiseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
