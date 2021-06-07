c2fbffdb9ef7150af641c07ba41f137d65a3dfab
Michael Stivens Pardo Burbano <programador2@sypsolutions.com>
2021-06-07 11:33:04 -0500

    example promise basic

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConsoleReporter } from 'jasmine';
 
 @Component({
   selector: 'app-pages',
@@ -11,6 +12,18 @@ export class PagesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const promise = new Promise((resolve, reject) => {
+      if (true) 
+        resolve('Hello word');
+      else
+        reject('Algo salio mal');
+    });
+    promise
+    .then((message) => {
+      console.log(message);
+    })
+    .catch(error => console.error('Error in my promise', error));
+    
+    console.log('End of Init');
   }
-
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -8,6 +8,8 @@ import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
+import { PromiseComponent } from './promise/promise.component';
+
 
 
 @NgModule({
@@ -15,7 +17,8 @@ import { SharedModule } from '../shared/shared.module';
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    PromiseComponent,
   ],
   exports: [
     DashboardComponent,
